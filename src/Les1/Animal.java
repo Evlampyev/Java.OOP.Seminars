8637eecf1a2a697d788b997133b1270f4418c31d
Bad_S <Evlampyev.Alex@gmail.com>
2023-03-06 23:31:57 +0500

    part 1

--- src/Les1/Animal.java
+++ src/Les1/Animal.java
@@ -1,2 +1,66 @@
-package Les1;public class animal {
+package Les1;
+
+import java.sql.SQLSyntaxErrorException;
+
+public class Animal {
+    public String name;
+    private String color;
+    private int pawsCount;
+
+    public Animal(String name, String color, int pawsCount) {
+        this.name = name;
+        this.color = color;
+        this.pawsCount = pawsCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getPawsCount() {
+        return pawsCount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPawsCount(int pawsCount) {
+        this.pawsCount = pawsCount;
+    }
+
+    public Animal(String name) {
+        this(name, null, 0);
+    }
+
+    public void toPlay() {
+        System.out.println("Played");
+    }
+
+    public void goToSleep() {
+        System.out.println("Already aspeep...");
+    }
+
+    public void speak() {
+        System.out.println("Yarr!!!");
+    }
+
+    public void eat() {
+        System.out.println("Ate...");
+    }
+
+    public void findFood() {
+        System.out.println("Found food...");
+    }
+    public void wakeUp () {
+        System.out.println("I am wake up!");
+    }
 }
--- src/Les1/Main.java
+++ src/Les1/Main.java
@@ -1,5 +1,18 @@
+package Les1;
+
 public class Main {
+    /**
+     * Main function to run
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal(null, null, 1);
+        System.out.println(animal.getColor());
+
+        Animal animal1 = new Animal("Bars");
+        System.out.println(animal1.name);
+
+
     }
 }
